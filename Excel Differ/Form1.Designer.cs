1dfa2ae028c1b4c9e22f6fb6053ea4d697224382
ovchinnikov.dk <danil42worker@gmail.com>
2019-12-07 08:52:56 +0700

    Init project

---
+++ Excel Differ/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace Excel_Differ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Diff = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(467, 40);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "File One";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(467, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(114, 48);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "File Two";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(22, 40);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(369, 48);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(22, 138);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(369, 48);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Diff
+            // 
+            this.Diff.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Diff.Location = new System.Drawing.Point(22, 237);
+            this.Diff.Name = "Diff";
+            this.Diff.Size = new System.Drawing.Size(559, 48);
+            this.Diff.TabIndex = 4;
+            this.Diff.Text = "Diff";
+            this.Diff.UseVisualStyleBackColor = true;
+            this.Diff.Click += new System.EventHandler(this.Diff_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(623, 297);
+            this.Controls.Add(this.Diff);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(639, 336);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(639, 336);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Excel Differ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Diff;
+    }
+}
+
---
+++ Excel Differ/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows.Forms;
+
+namespace Excel_Differ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string _fileOne = "";
+        private string _fileTwo = "";
+
+        private string OpenFileToFileName()
+        {
+            string fileName = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+            }
+
+            return fileName;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _fileOne = OpenFileToFileName();
+            textBox1.Text = _fileOne;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _fileTwo = OpenFileToFileName();
+            textBox2.Text = _fileTwo;
+        }
+
+        private List<string> ReadFirstLineFromExcelFile(string path)
+        {
+            var excelApp = new Excel.Application {Visible = true};
+            var excelWorkbooks = excelApp.Workbooks;
+            var excelWorkbook = excelWorkbooks.Open(path);
+            var excelSheets = excelWorkbook.Sheets;
+            var excelWorksheet = (Excel.Worksheet) excelSheets.Item[1];
+
+            bool isRead = true;
+            int index = 1;
+            var list = new List<string>();
+
+            while (isRead)
+            {
+                var p = (string) (excelWorksheet.Cells[1, index] as Excel.Range)?.Value;
+
+                if (p == null)
+                {
+                    isRead = false;
+                }
+                else
+                {
+                    list.Add(p);
+                    index++;
+                }
+            }
+
+            excelWorkbook.Close();
+            excelApp.Quit();
+
+            return list;
+        }
+
+        private Tuple<List<string>, List<string>> DiffList(List<string> fileOne, List<string> fileTwo)
+        {
+            var include = new List<string>();
+            var notInclude = new List<string>();
+            var items = new SortedSet<string>();
+
+            foreach (string itemOne in fileOne)
+            {
+                items.Add(itemOne);
+            }
+
+            foreach (string itemTwo in fileTwo)
+            {
+                items.Add(itemTwo);
+            }
+
+            foreach (string item in items)
+            {
+                if (fileOne.Contains(item) && fileTwo.Contains(item))
+                {
+                    include.Add(item);
+                }
+                else
+                {
+                    notInclude.Add(item);
+                }
+            }
+
+            return new Tuple<List<string>, List<string>>(include, notInclude);
+        }
+
+        private void SaveReportToExcel(Tuple<List<string>, List<string>> report)
+        {
+            var excelApp = new Excel.Application {Visible = true};
+            var excelWorkbooks = excelApp.Workbooks;
+            var excelWorkbook = excelWorkbooks.Add();
+            var excelSheets = excelWorkbook.Sheets;
+            var excelWorksheet = (Excel.Worksheet) excelSheets.Item[1];
+
+            var indexInclude = 1;
+            foreach (var value in report.Item1)
+            {
+                excelWorksheet.Cells[1, indexInclude] = value;
+                indexInclude++;
+            }
+
+            var indexNotInclude = 1;
+            foreach (var value in report.Item2)
+            {
+                excelWorksheet.Cells[2, indexNotInclude] = value;
+                indexNotInclude++;
+            }
+        }
+
+        private void Diff_Click(object sender, EventArgs e)
+        {
+            if (_fileOne != "" && _fileTwo != "")
+            {
+                var fileOne = ReadFirstLineFromExcelFile(_fileOne);
+                var fileTwo = ReadFirstLineFromExcelFile(_fileTwo);
+
+                var report = DiffList(fileOne, fileTwo);
+
+                SaveReportToExcel(report);
+            }
+            else
+            {
+                MessageBox.Show(@"Не выбраны файлы для сравнения");
+            }
+        }
+    }
+}
